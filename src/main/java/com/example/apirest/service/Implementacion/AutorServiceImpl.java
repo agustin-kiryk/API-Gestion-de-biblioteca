c16ddfbd9054874fad09fcc6b446d78c47f2c3ca
agustin-kiryk <agustin.kiryk@gmail.com>
2022-10-02 00:32:52 -0300

    agregando mappers, revisar autor y localidad ServiceImpl

--- src/main/java/com/example/apirest/service/Implementacion/AutorServiceImpl.java
+++ src/main/java/com/example/apirest/service/Implementacion/AutorServiceImpl.java
@@ -1,31 +1,53 @@
 package com.example.apirest.service.Implementacion;
 
 import com.example.apirest.DTO.AutorDTO;
+import com.example.apirest.entity.AutorEntity;
+import com.example.apirest.exception.ParamNotFound;
+import com.example.apirest.mapper.AutorMapper;
+import com.example.apirest.repository.AutorRepository;
 import com.example.apirest.service.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AutorServiceImpl implements AutorService {
+    @Autowired
+    AutorRepository autorRepository;
+    @Autowired
+    AutorMapper autorMapper;
     @Override
     public List<AutorDTO> findAll() {
-        return null;
+        List<AutorEntity> entities = this.autorRepository.findAll();
+        List<AutorDTO> result = this.autorMapper.autorListEntity2DTOList(entities);
+        return result;
     }
-
     @Override
-    public AutorDTO save(AutorDTO autor) {
-        return null;
+    public AutorDTO save(AutorDTO dto) {
+        AutorEntity entity = this.autorMapper.autorDTO2Entity(dto);
+        AutorEntity entitySaved = this.autorRepository.save(entity);
+        AutorDTO result = this.autorMapper.autorEntity2DTO(entitySaved);
+        return result;
     }
 
     @Override
     public AutorDTO findById(Long id) {
-        return null;
+        AutorEntity entity = this.autorRepository.findById(id).orElseThrow(
+                () -> new ParamNotFound("El ID del autor no existe"));
+        AutorDTO dto = this.autorMapper.autorEntity2DTO(entity);
+        return dto;
     }
 
     @Override
-    public AutorDTO update(Long id, AutorDTO autor) {
-        return null;
+    public AutorDTO update(Long id, AutorDTO dto) {
+        AutorEntity entity = this.autorRepository.findById(id).orElseThrow(
+                () -> new ParamNotFound("El ID del autor no existe"));
+        AutorEntity entityUpdate = autorMapper.autorDTO2Entity(dto);
+        entityUpdate.setId(entity.getId());
+        AutorEntity entitSaved = this.autorRepository.save(entityUpdate);
+        AutorDTO result = this.autorMapper.autorEntity2DTO(entitSaved);
+        return result;
     }
 
     @Override
